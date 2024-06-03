c26f26e419d3818ba12d83182a9c6636ea511b4d
shinjiyun <ranji12@naver.com>
2024-06-03 12:58:33 +0900

    forEach 기본형

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 //배열을 초기화 한 다음에 있는 배열 가지고 반복문 돌려보는 연습
 const all =[];
 const array = ["신지윤","김미연", "지하수"];
-array.forEach((arr)=>all.push(arr))
+array.forEach((arr)=>{
+  all.push(arr)
+});
 console.log(all);
