c8b194a623fff549d9e838c1880347b87457ea83
shinjiyun <ranji12@naver.com>
2024-06-03 15:36:37 +0900

    옛날 방식 객체 생성법과 지금의 객체 생성법

--- old.js
+++ old.js
@@ -1,3 +1,16 @@
 function old(a,b){
-  
+  this.first = a;
+  this.second = b;
 }
+let test = new old(1,2);
+console.log(test);
+
+
+class NewOld{
+  constructor(a,b){
+    this.first=a;
+    this.second=b;
+  }
+}
+const newStyle = new NewOld(1,2);
+console.log(newStyle);
