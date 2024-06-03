28feb44a82244009dc9774c37d6d5308937e27a9
shinjiyun <ranji12@naver.com>
2024-06-03 16:03:28 +0900

    안의 규격의 코드를 보는게 아니다. 제대로 작성해서 필요할때마다 꺼내 쓰는 것이다. set,get활용한 커피코드

--- coffee-constructor.js
+++ coffee-constructor.js
@@ -5,7 +5,45 @@ class MakeCoffee{
     this.sugar = sugar;
     this.milk = milk;
   }
+  //************************규격만들기*********
+  set been(value){
+    if(typeof(value)==='string'){
+      this._been = value; //_ : 새로운 키에 넣는 개념 (무언가 세팅을 통과한 값)
+    }else{
+      console.error("been는 문자열이 아닙니다.");
+    }
+  }
+  set water(value){
+    if(typeof(value)==='number'){
+      this._water=value;
+    }else{
+      console.error("water는 숫자형이 아닙니다.");
+    }
+  }
+  set sugar(value){
+    if(typeof(value)==="boolean"){
+      this._sugar=value;
+    }else{
+      console.error("sugar는 boolean이 아닙니다.");
+    }
+  }
+  
+  get been(){
+    return this._been+"산";
+  }
+  get water(){
+    return this._water+"mL";
+  }
+  get sugar(){
+    if(this._sugar === false){
+      return "쓴맛"
+    }else if(this._sugar === true){
+      return "단맛"
+    }
+  }
 }
-
 const cofee = new MakeCoffee("과테말라", 150, false, true);
 console.log(cofee);
+console.log(cofee.been);
+console.log(cofee.water);
+console.log(cofee.sugar);
